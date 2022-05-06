0d0de553d4a3109d11e2b1c0baef1531803cb758
Niklas Nygård <niklas.nygard01@gmail.com>
2022-05-06 11:54:29 +0300

    fixes for functions

--- polls.py
+++ polls.py
@@ -1,4 +1,3 @@
-from random import randint
 from database import db
 from flask import render_template, request, redirect
 
@@ -24,12 +23,11 @@ def create_poll(topic, creator_id):
     
     if "image" in request.form:
         image = request.form["image"]
-        sql_ = "INSERT INTO choices (image) VALUES (bytea(:image))"
-        db.session.execute(sql_, {"image":image})
+        sql_ = "INSERT INTO choices (poll_id, image) VALUES (:poll_id, :image)"
+        db.session.execute(sql_, {"poll_id":poll_id, "image":image})
 
     db.session.commit()
     return poll_id
-
 
 
 def get_poll_topic(id):
@@ -45,7 +43,7 @@ def get_poll_choices(id):
     return choices
 
 def get_picture(id):
-    sql = "SELECT id, image FROM choices WHERE poll_id=:id"
+    sql = "SELECT ENCODE(image,'base64') as base64 FROM choices WHERE poll_id=:id"
     result = db.session.execute(sql, {"id":id})
     image = result.fetchone()[0]
     return image
@@ -54,34 +52,17 @@ def get_choice(choice_id):
     sql = "SELECT choice, correct_choice FROM choices WHERE id=:choice_id"
     return db.session.execute(sql, {"choice_id":choice_id}).fetchone()
 
-def send_answer(choice_id, answer, user_id):
-    sql = "SELECT correct_choice FROM choices WHERE id=:id"
-    correct = db.session.execute(sql, {"id":choice_id}).fetchone()[0]
-    result = 1 if answer == correct else 0
-
-    sql = """INSERT INTO answers (user_id, choice_id, sent_at, result)
-            VALUES (:user_id, :choice_id, NOW(), :result)"""
-    db.session.execute(sql, {"user_id":user_id, "choice_id":choice_id, "result":result})
+def correct_answer(id):
+    sql = "SELECT correct_choice FROM choices WHERE poll_id=:id and choice=''"
+    correct = db.session.execute(sql, {"id":id}).fetchone()[0]
     db.session.commit()
-    
-
-#def result(id):
-#    sql = "SELECT topic FROM polls WHERE id=:id"
-#    result = db.session.execute(sql, {"id":id})
-#    topic = result.fetchone()[0]
-#    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
-#          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
-#    result = db.session.execute(sql, {"poll_id":id})
-#    choices = result.fetchall()
-#    return render_template("result.html", topic=topic, choices=choices)
-
+    return correct
 
 def remove_poll(poll_id, user_id):
     sql = "UPDATE polls SET visible=0 WHERE id=:id AND creator_id=:user_id"
     db.session.execute(sql, {"id":poll_id, "user_id":user_id})
     db.session.commit()
     
-
 def my_polls(user_id):
     sql = "SELECT id, topic FROM polls WHERE creator_id=:user_id AND visible=1 ORDER BY topic"
     return db.session.execute(sql, {"user_id":user_id}).fetchall()
